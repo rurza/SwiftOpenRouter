61aa12c724d7ea7568e0dbff7637091a0bd0ed92
Adam Różyński <adam@micropixels.pl>
2025-10-22 16:12:44 +0200

    Add supportedParameters

--- Sources/SwiftOpenRouter/Responses/ListAvailableModelsResponse.swift
+++ Sources/SwiftOpenRouter/Responses/ListAvailableModelsResponse.swift
@@ -11,8 +11,9 @@ public struct OpenRouterModel: Decodable, Sendable {
         case topProvider = "top_provider"
         case pricing
         case perRequestLimits = "per_request_limits"
+        case supportedParameters = "supported_parameters"
     }
-
+    
     public let id: String
     public let name: String
     public let created: Date
@@ -22,24 +23,56 @@ public struct OpenRouterModel: Decodable, Sendable {
     public let topProvider: TopProvider
     public let pricing: Pricing
     public let perRequestLimits: [String: String]?
-
+    public let supportedParameters: Set<Parameter>
+    
     public struct Architecture: Decodable, Sendable {
         public let modality: String
         public let tokenizer: String
     }
-
+    
     public struct TopProvider: Decodable, Sendable {
         enum CodingKeys: String, CodingKey {
             case contextLength = "context_length"
             case maxCompletionTokens = "max_completion_tokens"
             case isModerated = "is_moderated"
         }
-
+        
         public let contextLength: Int?
         public let maxCompletionTokens: Int?
         public let isModerated: Bool
     }
-
+    
+    public enum Parameter: Hashable, Decodable, Sendable {
+        case known(Key)
+        case unknown(String)
+        
+        public enum Key: String, Codable, Sendable {
+            case logprobs
+            case maxTokens = "max_tokens"
+            case reasoning
+            case responseFormat = "supported_parameters"
+            case seed
+            case stop
+            case structuredOutputs = "structured_outputs"
+            case temperature
+            case toolChoice = "tool_choice"
+            case tools
+            case webSearchOptions = "web_search_options"
+        }
+        
+        public init(from decoder: any Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            let value: Key? = try? container.decode(Key.self)
+            switch value {
+            case let .some(key):
+                self = .known(key)
+            case .none:
+                let unknownValue = try container.decode(String.self)
+                self = .unknown(unknownValue)
+            }
+        }
+    }
+    
     public struct Pricing: Decodable, Sendable {
         enum CodingKeys: String, CodingKey {
             case prompt
@@ -51,7 +84,7 @@ public struct OpenRouterModel: Decodable, Sendable {
             case webSearch = "web_search"
             case internalReasoning = "internal_reasoning"
         }
-
+        
         public let prompt: Decimal
         public let completion: Decimal
         public let image: Decimal?
